fd91b57aeea752566bbd91542fcc15d0a8511ba5
Minghao Liu <molikto@gmail.com>
2020-07-03 11:04:51 +0800

    11

--- src/editor/widget.rs
+++ src/editor/widget.rs
@@ -12,6 +12,9 @@ use std::ops::DerefMut;
 
 struct TokenLayout {
     token: Token,
+    margin_left: f64,
+    margin_right: f64,
+    is_separator: bool,
     layout: PietTextLayout,
 }
 
@@ -24,14 +27,57 @@ impl TokenLayout {
 // should always be non-empty
 struct Line {
     indent: f64,
-    tokens: Vec<TokenLayout>,
+    tokens: Vec<(TokenLayout, f64)>,
+    width: f64
 }
 
 
 impl Line {
-    fn width(&self) -> f64 {
-        let remaining: f64 = self.tokens.iter().map(|n| n.width()).sum();
-        self.indent + remaining
+    fn new() -> Line {
+        Line { indent: 0.0, tokens: vec![],  width: 0.0 }
+    }
+
+    fn single(token: TokenLayout) -> Line {
+        let width = token.width();
+        Line { indent: 0.0, tokens: vec![(token, 0.0)], width }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.tokens.is_empty()
+    }
+
+    fn push(&mut self, t: TokenLayout) {
+        let last = self.tokens.last();
+        let mut pre_margin = last.map(|n| n.0.margin_right).unwrap_or(0.0);
+        let mut pre_sep = last.map(|n| n.0.is_separator).unwrap_or(false);
+        let margin= if t.is_separator {
+            if pre_sep {
+                0.0 // two sep don't have margin
+            } else {
+                t.margin_left
+            }
+        } else {
+            if pre_sep {
+                pre_margin
+            } else {
+                t.margin_left.max(pre_margin)
+            }
+        };
+        self.width += margin + t.width();
+        self.tokens.push((t, margin));
+    }
+
+    fn append(&mut self, mut other: Line) {
+        if !other.is_empty() {
+            let (t, m) = other.tokens.remove(0);
+            self.push(t);
+            self.tokens.last_mut().unwrap().1 = m;
+            self.tokens.append(&mut other.tokens);
+        }
+    }
+
+    fn width(&mut self) -> f64 {
+        self.width
     }
 }
 
@@ -41,12 +87,16 @@ struct Block {
 }
 
 impl Block {
-    fn width(&self) -> f64 {
-        self.lines.iter().map(|n| n.width()).max_by(|a, b| a.partial_cmp(b).unwrap()).unwrap()
+    fn width(&mut self) -> f64 {
+        let mut w: f64 = 0.0;
+        for l in &mut self.lines {
+            w = w.max(l.width())
+        }
+        w
     }
 
     fn new() -> Block {
-        Block { lines: vec![Line { indent: 0.0, tokens: vec![] }] }
+        Block { lines: vec![Line::new()] }
     }
 
     fn wrap(mut self) -> LayoutResult {
@@ -59,7 +109,7 @@ impl Block {
 
     fn append_block(&mut self, mut b: Block) {
         let mut last = &mut self.lines.last_mut().unwrap();
-        if last.tokens.is_empty() {
+        if last.is_empty() {
             self.lines.remove(self.lines.len() - 1);
         }
         self.lines.append(&mut b.lines);
@@ -68,9 +118,9 @@ impl Block {
         let mut last = &mut self.lines.last_mut().unwrap();
         match res {
             LayoutResult::Single(a) => {
-                last.tokens.push(a)
+                last.push(a)
             }
-            LayoutResult::Line(mut l) => last.tokens.append(&mut l.tokens),
+            LayoutResult::Line(mut l) => last.append(l),
             LayoutResult::Block(mut b) => {
                 self.append_block(b)
             }
@@ -84,7 +134,7 @@ impl Block {
     }
 
     fn nl(&mut self) {
-        self.lines.push(Line { indent: 0.0, tokens: vec![] })
+        self.lines.push(Line::new())
     }
 }
 
@@ -95,7 +145,7 @@ enum LayoutResult {
 }
 
 impl LayoutResult {
-    fn width(&self) -> f64 {
+    fn width(&mut self) -> f64 {
         match self {
             LayoutResult::Single(t) => t.width(),
             LayoutResult::Line(l) => l.width(),
@@ -105,7 +155,7 @@ impl LayoutResult {
 
     fn to_lines(self) -> Vec<Line> {
         match self {
-            LayoutResult::Single(t) => vec![Line { indent: 0.0, tokens: vec![t] }],
+            LayoutResult::Single(t) => vec![Line::single(t)],
             LayoutResult::Line(l) => vec![l],
             LayoutResult::Block(b) => b.lines
         }
@@ -134,7 +184,7 @@ impl EditorState {
             Token::new(1, "{"),
             Token::new(7, "key"),
             Token::new(4, ":"),
-            Token::new(10, "1000"),
+            Token::new(8, "1000"),
             Token::new(2, ","),
             Token::new(7, "key2 "),
             Token::new(4, ":"),
@@ -157,16 +207,16 @@ impl EditorState {
 fn style(tp: TokenType) -> Color {
     match tp {
         TokenType::Delimiter => {
-            Color::rgb8(169, 183, 198)
+            Color::rgb8(0, 183, 198)
         },
         TokenType::Separator => {
-            Color::rgb8(169, 183, 198)
+            Color::rgb8(169, 0, 198)
         }
         TokenType::Keyword => {
-            Color::rgb8(106, 135, 89)
+            Color::rgb8(204, 120, 55)
         },
         TokenType::Const => {
-            Color::rgb8(204, 120, 55)
+            Color::rgb8(106, 135, 89)
         },
         TokenType::Unspecified => {
             Color::rgb8(169, 183, 198)
@@ -203,17 +253,17 @@ impl Widget<u64> for EditorState {
             let tokens = &line.tokens;
             let mut ascent: f64 = 0.0;
             let mut descent_and_leading: f64 = 0.0;
-            for token in tokens {
+            for (token, _) in tokens {
                 let metrics = token.layout.line_metric(0).unwrap();
                 ascent = ascent.max(metrics.baseline);
                 descent_and_leading = descent_and_leading.max(metrics.height - metrics.baseline);
             }
             let height = ascent + descent_and_leading;
-            for token in tokens {
+            for (token, margin) in tokens {
                 let width = token.layout.width();
                 //println!("{}, {}", width, height);
                 ctx.draw_text(&token.layout, Point::new(left, top + ascent), &style(self.language.token_type(token.token.tp)));
-                left += width;
+                left += width + margin;
             }
             top += height;
         }
@@ -227,14 +277,22 @@ struct LayoutParams<'a, 'c> {
 }
 
 impl LayoutParams<'_, '_> {
-    fn layout_token(&mut self, node: Node) -> LayoutResult {
+    fn layout_token(&mut self, node: Node, tp: TokenType) -> LayoutResult {
         let token = self.state.tokens[node.start_byte()].clone();
         let layout = self.ctx.new_text_layout(
             self.state.font.as_ref().unwrap(),
             &token.str,
             f64::MAX,
         ).build().unwrap();
-        LayoutResult::Single(TokenLayout { token, layout })
+        let is_sep = match tp { TokenType::Separator => true, _ => false };
+        let margin = if is_sep { 0.0 } else { 8.0 };
+        LayoutResult::Single(TokenLayout {
+            token,
+            margin_left: margin,
+            margin_right: margin,
+            is_separator: is_sep,
+            layout
+        })
     }
 
     fn layout_node(&mut self, node: Node, max_width: f64) -> LayoutResult {
@@ -295,19 +353,19 @@ impl LayoutParams<'_, '_> {
                     }
                     LayoutResult::Block(block)
                 } else {
-                    let mut tokens: Vec<TokenLayout> = vec![];
+                    let mut line = Line::new();
                     for (_, child) in children_layout {
                         match child {
                             LayoutResult::Single(a) => {
-                                tokens.push(a);
+                                line.push(a);
                             }
                             LayoutResult::Line(mut b) => {
-                                tokens.append(&mut b.tokens);
+                                line.append(b);
                             }
                             _ => panic!("not possible")
                         }
                     }
-                    LayoutResult::Line(Line { indent: 0.0, tokens })
+                    LayoutResult::Line(line)
                 }
             },
             NodeType::Unspecified => {
@@ -321,13 +379,13 @@ impl LayoutParams<'_, '_> {
                     let child_max_width = max_width - current_width;
                     let mut layout = self.layout_node(node, child_max_width);
                     block.append(layout);
-                    current_width = block.lines.last().unwrap().width();
+                    current_width = block.lines.last_mut().unwrap().width();
                     has_child = cursor.goto_next_sibling();
                 }
                 block.wrap()
             },
-            NodeType::Token(_) => {
-                self.layout_token(node)
+            NodeType::Token(tp) => {
+                self.layout_token(node, tp)
             },
         }
     }
